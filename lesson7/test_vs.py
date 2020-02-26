4d3680e55ae019f95a52bac2dd821262afa7f784
LeWsha01 <58860425+LeWsha01@users.noreply.github.com>
2020-02-26 22:59:21 +0300

    Update test_vs.py

--- lesson7/test_vs.py
+++ lesson7/test_vs.py
@@ -1,40 +1,48 @@
 class Department:
     class BudgetError(ValueError):
         pass
-    lst = []
-    def __init__(self, name, budget, employees={}):
+
+    def __init__(self, name, employees, budget):
         self.name = name
         self.employees = employees
         self.budget = budget
 
-    def __str__(self):
-        lst = [i for i in self.employees.keys()]
-        return f'Name of department:{self.name}(workers: {len(lst)}  - average selary: {self.get_average_selary()}), budget: {self.budget}'
-
-    def __repr__(self): 
-        return str(self)
-
-    def get_average_selary(self):
-        lst = [i for i in self.employees.values()]
-        return round(sum(lst) / len(lst), 2) if sum(lst) else None
-
     def get_budget_plan(self):
-        sal = [i for i in self.employees.values()]
-        return (self.budget - sum(sal))
+        result = self.budget - sum(self.employees.values())
+        if result < 0:
+            raise self.BudgetError
+        return result
 
-    # def merge_departments(*args):
-    #     return sum(args)
+    def get_average_salary(self):
+        return round(
+            sum(self.employees.values()) / len(self.employees),
+            2
+        )
+
+    def merge_departments(self, *departments):
+        names = []
+        new_budget = 0
+        new_employees = {}
+
+        for department in departments:
+            names.append(department.name)
+            new_budget += department.budget
+            new_employees.update(department.employees)
+        new_name = ' - '.join(names)
+
+        if new_budget < sum(new_employees.values()):
+            raise self.BudgetError
+        return Department(new_name, new_employees, new_budget)
 
     def __add__(self, other):
-        return Department(
-            f'{self.name} - {other.name}',
-            self.budget + other.budget,
-            {**self.employees,**other.employees}
-        )    
+        return self.merge_departments(self, other)
 
     def __or__(self, other):
-        return self if self.get_budget_plan > other.get_budget_plan else other
-         
+        plan_1 = self.budget - sum(self.employees.values())
+        plan_2 = other.budget - sum(other.employees.values())
+        if plan_1 < 0 or plan_2 < 0:
+            raise self.BudgetError
+        return self if plan_1 >= plan_2 else other
         
 dep1 = Department("bsu", 1500, {'vlad': 1000, 'nik': 400})
 dep2 = Department("qqq", 5555, {'ivan': 100, 'john': 140})
